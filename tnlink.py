f7ebedcd28b26b3a18a7180a4193bcc2871f9209
Jack <92579700+JohnWickKeanue@users.noreply.github.com>
2022-12-28 22:15:29 +0530

    Update tnlink.py

--- tnlink.py
+++ tnlink.py
@@ -1,44 +1,15 @@
+import requests
 
-import re
-import time
-import cloudscraper
-from bs4 import BeautifulSoup 
-
-url = "https://link.tnlink.in/TS95D7"  #@param {type:"string"}
+url = "https://tnlink.in/SCPI"  #@param {type:"string"}
 
 
 def tnlink(url):
-    
-    client = cloudscraper.create_scraper(allow_brotli=False)
-    
-    
-    DOMAIN = "https://gadgets.usanewstoday.club"
-
-    url = url[:-1] if url[-1] == '/' else url
-
+    client = requests.session()
+    DOMAIN = "https://gadgets.earnme.club"
     code = url.split("/")[-1]
-    
     final_url = f"{DOMAIN}/{code}"
-    
-    ref = "https://usanewstoday.club/"
-    
-    h = {"referer": ref}
-  
-    resp = client.get(final_url,headers=h)
-    
-    soup = BeautifulSoup(resp.content, "html.parser")
-    
-    inputs = soup.find_all("input")
-   
-    data = { input.get('name'): input.get('value') for input in inputs }
-
-    h = { "x-requested-with": "XMLHttpRequest" }
-    
-    time.sleep(8)
-    r = client.post(f"{DOMAIN}/links/go", data=data, headers=h)
-    try:
-        return r.json()['url']
-    except: return "Something went wrong :("
+    download = requests.get(final_url, stream=True, allow_redirects=False)
+    return download.headers["location"]
     
 # ---------------------------------------------------------------------------------------------------------------------
 print(tnlink(url))
