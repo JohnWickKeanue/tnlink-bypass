2c24ada7843e28b7a748b1fbe4db58be78f88680
Jack <92579700+JohnWickKeanue@users.noreply.github.com>
2023-01-03 10:43:13 +0530

    Update tnlink.py

--- tnlink.py
+++ tnlink.py
@@ -1,15 +1,43 @@
-import requests
 
-url = "https://tnlink.in/SCPI"  #@param {type:"string"}
+import time
+import requests
+from bs4 import BeautifulSoup 
+
+url = "https://link.tnlink.in/TS95D7"  #@param {type:"string"}
 
 
 def tnlink(url):
+    
     client = requests.session()
-    DOMAIN = "https://gadgets.earnme.club"
+    
+    
+    DOMAIN = "https://gadgets.usanewstoday.club"
+
+    url = url[:-1] if url[-1] == '/' else url
+
     code = url.split("/")[-1]
+    
     final_url = f"{DOMAIN}/{code}"
-    download = requests.get(final_url, stream=True, allow_redirects=False)
-    return download.headers["location"]
+    
+    ref = "https://usanewstoday.club/"
+    
+    h = {"referer": ref}
+  
+    resp = client.get(final_url,headers=h)
+    
+    soup = BeautifulSoup(resp.content, "html.parser")
+    
+    inputs = soup.find_all("input")
+   
+    data = { input.get('name'): input.get('value') for input in inputs }
+
+    h = { "x-requested-with": "XMLHttpRequest" }
+    
+    time.sleep(8)
+    r = client.post(f"{DOMAIN}/links/go", data=data, headers=h)
+    try:
+        return r.json()['url']
+    except: return "Something went wrong :("
     
 # ---------------------------------------------------------------------------------------------------------------------
 print(tnlink(url))
